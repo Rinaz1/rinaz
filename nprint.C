25cd426b06bcf52fdd26aca8e64e9c20df000437
Rinaz1 <35994961+Rinaz1@users.noreply.github.com>
2018-02-02 14:19:42 +0530

    Create nprint.C

---
+++ nprint.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char hello;
+int i;
+printf("how times printed");
+scanf("%d",i);
+for(i=1;i<10;i++)
+printf("%c",hello);
+getch();
+return 0;
+}
