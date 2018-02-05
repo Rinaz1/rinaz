7cfebedb1dc334c74e79c6c28f04c2f4719e32c5
Rinaz1 <35994961+Rinaz1@users.noreply.github.com>
2018-02-05 16:03:52 +0530

    Create nvalue.C

---
+++ nvalue.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,sum,n;
+{
+printf("enter the value\n");
+for(i=0;i<=n;i++);
+scanf("%d",&n);
+}
+sum=0;
+for(i=0;i<=n;i++)
+sum=sum+i;
+printf("the sum of %d",sum);
+getch();
+return 0;
+}
