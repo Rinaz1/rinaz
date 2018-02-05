0a55e51a7bdf46a25bac2b6a14173154f8bb10cd
Rinaz1 <35994961+Rinaz1@users.noreply.github.com>
2018-02-05 16:33:25 +0530

    Create count.C

---
+++ count.C
@@ -0,0 +1,17 @@
+include <stdio.h>
+int main() {
+int a;
+pintf("enter the value\n");
+scanf("%d",@a);
+if(a<10 && a>9)
+printf("the count is 1");
+if(a<100 && a>99)
+printf("the count is 2");
+if(a<1000 && a>999)
+printf("the count is 3");
+if(a<10000 && a>9999)
+printf("the count is 4");
+else 
+printf("its out of limit");
+return 0;
+}
