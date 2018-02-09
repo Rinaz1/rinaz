4029a6c855ffe7e2c304243d77c9a694e2a3357c
Rinaz1 <35994961+Rinaz1@users.noreply.github.com>
2018-02-09 14:29:24 +0530

    Create oddno.C

---
+++ oddno.C
@@ -0,0 +1,13 @@
+#include<stdio.>
+int main()
+{
+int a,b;
+printf("enter the value ");
+scanf("%d",a);
+for(i=a;i<=b;i++)
+sum=i%2;
+if(sum==0)
+printf("%d",i);
+else
+printf("this is prime no");
+}
