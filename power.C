90e6d1fd5f65de0101eb8f77855d678885df7438
Rinaz1 <35994961+Rinaz1@users.noreply.github.com>
2018-02-09 13:42:48 +0530

    Create power.C

---
+++ power.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("enter theb value");
+scanf("%d",&a);
+printf("enter the power\n");
+scaf("%d",&b);
+c=a^b;
+printf("the value is %d \n",c);
+getch();
+return 0;
+}
