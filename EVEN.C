7c755e5b5869fe74b5dca16154f5d0633d63c420
Rinaz1 <35994961+Rinaz1@users.noreply.github.com>
2018-02-12 15:45:38 +0530

    Create EVEN.C

---
+++ EVEN.C
@@ -0,0 +1,14 @@
+#include<stdio.>
+int main()
+{
+int a,b;
+printf("enter the value ");
+scanf("%d",a);
+for(i=a;i<=b;i++)
+sum=i%2;
+for(i=a;i<=b;i++)
+if(sum==0)
+printf("%d",i);
+else
+printf("this iS NOT HAVE");
+}
