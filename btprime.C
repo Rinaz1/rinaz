8a84ecbcc401f0ffac0616cbd595d3f706abc68c
Rinaz1 <35994961+Rinaz1@users.noreply.github.com>
2018-02-12 15:54:04 +0530

    Create btprime.C

---
+++ btprime.C
@@ -0,0 +1,15 @@
+#include<stdio.>
+int main()
+{
+int a,b;
+printf("enter the value ");
+scanf("%d",a);
+for(i=a;i<=b;i++)
+sum=i%2;
+c=i%i;
+for(i=a;i<=b;i++)
+if(sum==0 && i==0)
+printf("%d",i);
+else
+printf("this iS NOT HAVE");
+}
