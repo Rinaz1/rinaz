93f73d36d7f862756f7c365be98342e5e99ba289
Rinaz1 <35994961+Rinaz1@users.noreply.github.com>
2018-02-19 16:38:18 +0530

    Create intarr.C

---
+++ intarr.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a[r],b,c;
+printf("enter the no\n");
+scanf("%d",&a[r]);
+for(i=0;i<=a[r];i++)
+scanf("%d",&a[i);
+for(i=0;i<=a[r];i++)
+temp=a[0]
+getch();
+return 0;
+}
