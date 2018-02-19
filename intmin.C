83150129797de16c60e3063799adcfb0c64101a7
Rinaz1 <35994961+Rinaz1@users.noreply.github.com>
2018-02-19 16:28:20 +0530

    Create intmin.C

---
+++ intmin.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a[r],b,c;
+printf("enter the no\n");
+scanf("%d",&a[r]);
+for(i=0;i<=a[r];i++)
+scanf("%d",&a[i);
+temp=a[0];
+if(a[0]>a[r])
+printf("the minimum value is");
+getch();
+return 0;
+}
