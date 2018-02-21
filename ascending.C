f2e539903cd6ba6f0590a5e553f96c9d8c193986
Rinaz1 <35994961+Rinaz1@users.noreply.github.com>
2018-02-21 16:21:47 +0530

    Create ascending.C

---
+++ ascending.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+int a,temp,i,j,a[20];
+printf("enter the array of numbers");
+scanf("%d",&a);
+printf("enter the numbers");
+for(i=0;i<=a;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<=a;i++)
+{
+for(j=i+1;j<=a;j++)
+{
+if(a[i]>a[j])
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}
+getch();
+return 0;
+}
